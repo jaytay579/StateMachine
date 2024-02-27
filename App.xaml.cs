7ddb90580e4e3e5bd6f53ad37ef0d758ba56a790
jaytay579 <jasontaylor579@gmail.com>
2024-02-27 22:40:51 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StateMachine
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿namespace StateMachine
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        int state = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            trafficController();
+        }
+
+        private void trafficController()
+        {
+            if (state == 0)
+            {
+                countUp();
+                if (count == 10)
+                {
+                    state = 1;
+                }
+            } else
+            {
+                countDown();
+                if (count == 0)
+                {
+                    state = 0;
+                }
+            }
+        }
+
+        private void countUp()
+        {
+                count++;
+
+                if (count == 1)
+                    CounterBtn.Text = $"Clicked {count} time";
+                else
+                    CounterBtn.Text = $"Clicked {count} times";
+
+                SemanticScreenReader.Announce(CounterBtn.Text);
+            }
+        
+
+        private void countDown()
+        {
+            count--;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
